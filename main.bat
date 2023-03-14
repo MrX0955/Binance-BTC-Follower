bd032ace99200a972d3c837e60d7f262c925e318
Empty <77683874+MrX0955@users.noreply.github.com>
2023-03-15 02:52:50 +0300

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,28 @@
+pip install asyncio websockets pystyle colorama
+color 0a
+@echo off
+cls
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+timeout 5
+start https://cracked.io/Sweety666
+start https://github.com/MrX0955
